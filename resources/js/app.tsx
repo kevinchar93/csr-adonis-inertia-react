282ec86395538a32023bed1db4109b3036d52201
Kevin Charles <kevincharles018@gmail.com>
2023-01-26 00:59:03 +0000

    Create entry point app.tsx

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,1 +1,20 @@
+import { InertiaApp } from '@inertiajs/inertia-react'
+import React from 'react'
+import ReactDOM from 'react-dom'
 import '../css/app.css'
+
+// initial page object with props from server
+const root = document.getElementById('app')
+const page = JSON.parse(root.dataset.page)
+
+// dynamically load required page component from "resources/js/Pages/." dir
+async function resolver(pageName) {
+  const module = await import(`./Pages/${pageName}`)
+  return module.default
+}
+
+function App() {
+  return <InertiaApp initialPage={page} resolveComponent={resolver} initialComponent={''} />
+}
+
+ReactDOM.render(<App />, root)
